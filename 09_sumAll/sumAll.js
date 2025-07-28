57b771b7b6018d5e1e3d41bba851bd6507177904
eafranklin3991 <eafranklin3991@gmail.com>
2025-07-28 13:54:01 -0400

    Complete exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let result = [];
+    for (let i = start; i <= end; i++) {
+        result.push(i);
+    }
+    return result.reduce((total, currentItem) => {
+        return total + currentItem;
+    }, 0);
 };
 
 // Do not edit below this line
