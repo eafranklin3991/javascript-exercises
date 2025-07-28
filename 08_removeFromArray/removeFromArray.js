41f8c34d170500f8593731c49c9fafc724bbe613
eafranklin3991 <eafranklin3991@gmail.com>
2025-07-28 13:30:40 -0400

    Complete exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, removeMe) {
+    return arr.filter(item => item !== removeMe);
 };
+
+const arr = [1, 2, 3, 4];
 
 // Do not edit below this line
 module.exports = removeFromArray;
